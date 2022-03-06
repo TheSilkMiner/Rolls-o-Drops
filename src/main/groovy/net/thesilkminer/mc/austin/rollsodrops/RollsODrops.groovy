fd26672e4a9c23d1a8fe46dc0f6ac076b8a471a4
Niklas Wimmer <niklas.wim0@gmail.com>
2022-03-06 11:53:01 +0100

    Add gradle build files and sample mod class
    Does not run currently, due to an upstream error in APLP. As soon as
    that error is resolved, this setup should run just fine.
    
    Signed-off-by: Niklas Wimmer <niklas.wim0@gmail.com>

---
+++ src/main/groovy/net/thesilkminer/mc/austin/rollsodrops/RollsODrops.groovy
@@ -0,0 +1,10 @@
+package net.thesilkminer.mc.austin.rollsodrops
+
+import org.apache.logging.log4j.LogManager
+
+class RollsODrops {
+
+    RollsODrops() {
+        LogManager.getLogger(RollsODrops).info('It loads, Hurra!')
+    }
+}
